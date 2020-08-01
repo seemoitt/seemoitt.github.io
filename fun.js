14e63de88c522a236c3572791b8afcff041ad99a
seemoitt <seemoitt@users.noreply.github.com>
2020-08-01 02:39:30 +0100

    JS added
    fun.js solely for changing greeting on page load atm

---
+++ fun.js
@@ -0,0 +1,11 @@
+// choosing where to append
+let pageGreet = document.querySelector(".greeting");
+// create h1 node
+let greetHeading = document.createElement("H1");
+//create text for h1 node using array
+let greetings = ["hello","salut","greetings","ciao","hallo", "greetings, weary wanderer"];
+//rng for greeting select from array
+let chooseGreeting = document.createTextNode(greetings[Math.floor(Math.random()* greetings.length)]);
+//append text to h1, h1 to body
+greetHeading.appendChild(chooseGreeting);
+pageGreet.appendChild(greetHeading);
