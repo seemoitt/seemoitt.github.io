8798d3cf479041bce63ae602ab51473a5ea3e02a
seemoitt <seemoitt@users.noreply.github.com>
2020-08-01 17:55:04 +0100

    responsive site sizing

--- fun.js
+++ fun.js
@@ -3,7 +3,7 @@ let pageGreet = document.querySelector(".greeting");
 // create h1 node
 let greetHeading = document.createElement("H1");
 //create text for h1 node using array
-let greetings = ["hello","salut","greetings","ciao","hallo", "greetings, weary wanderer"];
+let greetings = ["hello","salut","greetings","ciao","hallo", "greetings, friend"];
 //rng for greeting select from array
 let chooseGreeting = document.createTextNode(greetings[Math.floor(Math.random()* greetings.length)]);
 //append text to h1, h1 to body
